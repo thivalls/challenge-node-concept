c280dd0be5c0110e109c842783a60b427e7abb74
valls <thiago@agenciatw.com.br>
2020-04-21 17:35:42 -0300

    resolving all tests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,71 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", async (request, response) => {
+  return await response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const repository = request.body;
+  repository.id = uuid();
+  repository.likes = 0;
+  repositories.push(repository);
+  return response.send(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  if (!isUuid(id)) {
+    return response.status(400).json({ message: "Invalid uuid" });
+  }
+  if(request.body.likes) {
+    return response.status(400).json({ likes: 0 })
+  }
+  const newRepository = request.body;
+  const targetRepository = repositories.find((repository) => {
+    return repository.id === id;
+  })
+  if (!targetRepository) {
+    return response.status(400).json({ message: "Repository not found" });
+  }
+  const updatedRepository = Object.assign(targetRepository, newRepository);
+
+  return response.json(updatedRepository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  if (!isUuid(id)) {
+    return response.status(400).json({ message: "Invalid uuid" });
+  }
+ 
+  const repositoryExist = repositories.findIndex((repository) => {
+    return repository.id === id;
+  })
+
+  if (repositoryExist < 0) {
+    return response.status(400).json({ message: "Repository not found" });
+  }
+
+  repositories.splice(repositoryExist, 1);
+
+  return response.status(204).json([]);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  if (!isUuid(id)) {
+    return response.status(400).json({ message: "Invalid uuid" });
+  }
+  const targetRepository = repositories.find((repository) => {
+    return repository.id === id;
+  })
+  if (!targetRepository) {
+    return response.status(400).json({ message: "Repository not found" });
+  }
+  targetRepository.likes += 1;
+
+  return response.json({ likes: targetRepository.likes})
 });
 
 module.exports = app;
